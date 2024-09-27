8cb82de88af5855c90e9d6897092533e02b1f3d5
Puvan <puvanes77@live.com.sg>
2024-09-27 13:20:06 +0800

    minor changes to simplify code

--- churn-attrition/src/components/Navbar.js
+++ churn-attrition/src/components/Navbar.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Container, Nav, Navbar, Breadcrumb } from "react-bootstrap";
+import { Container, Navbar, Breadcrumb, Card } from "react-bootstrap";
 import home from '../assets/img/home.png';
 
 
 export const NavBar = () => {
     return (
-        <Navbar expand="lg" className='container-fluid'>
-            <Container>
-                <Navbar.Brand href="#home" className="logo">
-                    <img src={home} alt="logo"></img>
+        <Navbar expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#home">
+                    <img src={home} alt="logo" className="logo-img"></img>
                 </Navbar.Brand>
+                <Breadcrumb>
+                    <Breadcrumb.Item active>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item href="#analysis">Form</Breadcrumb.Item>
+                </Breadcrumb>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto nav-underline">
-                    <Breadcrumb>
-                        <Breadcrumb.Item active>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item href="#analysis">Form</Breadcrumb.Item>
-                    </Breadcrumb>
-                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>

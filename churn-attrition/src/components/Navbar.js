c4b7da386c7fdeb5b25ab00d2d34eebd73d92451
Puvan <puvanes77@live.com.sg>
2024-10-07 16:22:35 +0800

    minor fixes to navbar

--- churn-attrition/src/components/Navbar.js
+++ churn-attrition/src/components/Navbar.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Container, Navbar, Breadcrumb, Form, Button } from "react-bootstrap";
 import home from '../assets/img/home.png';
 import searchBar from '../assets/img/searchIcon.png';
 
 
 export const NavBar = () => {
+    const [isActive, setActive] = useState('Home');
+
     return (
         <Navbar expand="lg">
             <Container fluid>
                 <Navbar.Brand href="#home">
                     <img src={home} alt="logo" className="logo-img"></img>
                 </Navbar.Brand>
-                <Breadcrumb>
-                    <Breadcrumb.Item active>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item href="#analysis">Form</Breadcrumb.Item>
+                <Breadcrumb className="breadcrumb">
+                    <Breadcrumb.Item active={isActive === 'Home'? true: false} 
+                    onClick={() => {
+                        setActive('Home');
+                    }} href="#home">Home</Breadcrumb.Item>
+                    <Breadcrumb.Item active={isActive === 'Form'? true: false} 
+                    onClick={ () => {
+                        setActive('Form');
+                    }} href="#analysis">Form</Breadcrumb.Item>
                 </Breadcrumb>
-                <Form className="d-flex">
+                <Form className="d-flex form">
                     <Form.Control
                         type="search"
                         placeholder="Search"

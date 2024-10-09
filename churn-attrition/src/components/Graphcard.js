cf13a7e5c76680db8335b7f04e7fc7dbf26b27b8
Puvan <puvanes77@live.com.sg>
2024-10-09 23:56:33 +0800

    mvp implementation of simple bar chart
    with simple churn logic too

--- churn-attrition/src/components/Graphcard.js
+++ churn-attrition/src/components/Graphcard.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import { Bar } from 'react-chartjs-2';
+import { Chart, CategoryScale, LinearScale, BarElement } from 'chart.js';
+
+Chart.register(CategoryScale, LinearScale, BarElement);
 
 export const GraphCard = () => {
     const [data, setData] = useState(null);
@@ -18,12 +22,27 @@ export const GraphCard = () => {
         );
     }
 
+    const chartData = {
+        labels: ['Churned Customers', 'Active Customers'],
+        datasets: [
+            {
+                label: 'Customer Count',
+                data: [data.churned_customers, data.active_customers],
+                backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(75, 192, 192, 0.2)'],
+                borderColor: ['rgba(255, 99, 132, 1)', 'rgba(75, 192, 192, 1)'],
+                borderWidth: 1,
+            },
+        ],
+    };        
+
+
     return (
         <Container fluid>
             <h1>Churn Attrition Dashboard Analytics</h1>
             <p>Total Cusotmers: {data.total_customers}</p>
             <p>Churned Customers: {data.churned_customers}</p>
             <p>Churn Rate: {data.churn_rate}</p>
+            <Bar data={chartData}/>
         </Container>
     )
 }

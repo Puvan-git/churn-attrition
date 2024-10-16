fa2c268f470d3b2d0a8e53dddccfa8dbb39d4076
Puvan <puvanes77@live.com.sg>
2024-10-16 23:11:41 +0800

    async function implementation error

--- churn-attrition/src/components/Graphcard.js
+++ churn-attrition/src/components/Graphcard.js
@@ -5,16 +5,33 @@ import CorrMatrix from './CorrMatrix';
 
 export const GraphCard = () => {
     const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const URL = "http://127.0.0.1:8000/api/churn_app/churn_analysis/";
 
     useEffect(() => {
-        // Fetch churn analysis results from Django backend
-        fetch('http://127.0.0.1:8000/api/churn_app/churn_analysis/')
-            .then(response => response.json())
-            .then( (data) => {
-                setData(data)
-                console.log(data.matrix)
-            })
-            .catch(error => console.error('Error fetching churn analysis: ', error));
+        setLoading(true);
+        
+        async function fetchData() {
+            try{
+                const response = await fetch(URL);
+                setData(response.json());
+                console.log(data);
+            } catch(e){
+                console.log('Error occured: ', e);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchData()
+        // // Fetch churn analysis results from Django backend
+        // fetch('http://127.0.0.1:8000/api/churn_app/churn_analysis/')
+        //     .then(response => response.json())
+        //     .then( (data) => {
+        //         setData(data)
+        //         console.log(data.matrix)
+        //     })
+        //     .catch(error => console.error('Error fetching churn analysis: ', error));
     }, []);
 
     if (!data) {
